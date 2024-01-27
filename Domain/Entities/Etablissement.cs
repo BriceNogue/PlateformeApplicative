b82241ceb6c89270d23d5fab3cde2016a1b4280f
BriceNogue <bricenogue@gmail.com>
2024-01-27 14:57:50 +0100

    Ajout de l'entite Etablissement, et de sa migration.

---
+++ Domain/Entities/Etablissement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Etablissement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Adress { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Pays { get; set; } = string.Empty;
+    }
+}
