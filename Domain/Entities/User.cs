12e5ba15cd3802737dbca9a1083747cc0d392aba
BriceNogue <bricenogue@gmail.com>
2024-01-27 12:48:52 +0100

    Creation de l'entite User

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,6 +6,7 @@ namespace Domain.Entities
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
