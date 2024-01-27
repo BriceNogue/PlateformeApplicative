7bc58333c25197ef78568aa94cf685e6c27a17d4
BriceNogue <bricenogue@gmail.com>
2024-01-27 12:59:46 +0100

    Ajout des packages necessaires pour la couche Domain de l'API.

---
+++ Domain/Repositories/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Repositories
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-ALPHA;Database=PlateformeApplicative;Trusted_Connection=true;TrustServerCertificate=true;")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+    }
+}
