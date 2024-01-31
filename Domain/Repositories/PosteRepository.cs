7d8939d264779071535e1b5c5ab86504971f026a
BriceNogue <bricenogue@gmail.com>
2024-01-31 14:15:23 +0100

    Creation du PosteRepository

---
+++ Domain/Repositories/PosteRepository.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public class PosteRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public PosteRepository() 
+        {
+            _dataContext = new DataContext();
+        }
+
+        public List<Poste> GetAll()
+        {
+            return _dataContext.Postes.ToList();
+        }
+
+        public Poste Get(int id)
+        {
+            return _dataContext.Postes.FirstOrDefault(p => p.Id == id)!;
+        }
+
+        public void Add(Poste post)
+        {
+            _dataContext.Postes.Add(post);
+            _dataContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var poste = Get(id);
+            _dataContext?.Postes.Remove(poste);
+        }
+
+        public void Update(Poste post)
+        {
+            _dataContext.Postes.Update(post);
+            _dataContext.SaveChanges();
+        }
+    }
+}
