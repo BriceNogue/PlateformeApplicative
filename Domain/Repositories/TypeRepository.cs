b018d43df19347c9ed0b457c1882a637d9616246
BriceNogue <bricenogue@gmail.com>
2024-01-31 11:59:33 +0100

    Creation du Repository TypeRepository

---
+++ Domain/Repositories/TypeRepository.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public class TypeRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public TypeRepository() 
+        {
+            _dataContext = new DataContext();
+        }
+
+        public List<TypeE> GetAll() 
+        {
+            return _dataContext.Types.ToList();
+        }
+
+        public TypeE Get(int id)
+        {
+            return _dataContext.Types.FirstOrDefault(t => t.Id == id)!;
+        }
+
+        public void Add(TypeE type)
+        {
+            _dataContext.Types.Add(type);
+            _dataContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var type = Get(id);
+            _dataContext.Remove(type);
+            _dataContext.SaveChanges();
+        }
+
+        public void Update(TypeE type)
+        {
+            _dataContext.Update(type);
+            _dataContext.SaveChanges();
+        }
+    }
+}
