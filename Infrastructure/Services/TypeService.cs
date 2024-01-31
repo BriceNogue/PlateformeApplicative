3770197297808b22247934a8facc947360878984
BriceNogue <bricenogue@gmail.com>
2024-01-31 20:28:27 +0100

    Creation du TypeService

--- Infrastructure/Services/TypeService.cs
+++ Infrastructure/Services/TypeService.cs
@@ -1,8 +1,74 @@
-﻿
+﻿using Domain.Repositories;
+using Domain.Entities;
+using Infrastructure.Modeles;
+
 namespace Infrastructure.Services
 {
     public class TypeService
     {
-        
+        private readonly TypeRepository _typeRepository;
+
+        public TypeService() 
+        {
+            _typeRepository = new TypeRepository();
+        }
+
+        public List<TypeE> GetAll()
+        {
+            return _typeRepository.GetAll();
+        }
+
+        public TypeE Get(int id)
+        {
+            return _typeRepository.Get(id);
+        }
+
+        public bool Add(TypeModele type)
+        {
+            if (type.Id > 0)
+            {
+                return false;
+            }
+            else
+            {
+                var typeE = new TypeE()
+                {
+                    Id = type.Id,
+                    Nom = type.Nom,
+                    Description = type.Description,
+                    ObjetConcerne = type.ObjetConcerne,
+                };
+
+                _typeRepository.Add(typeE);
+                return true;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            var existingType = _typeRepository.Get(id);
+            if (existingType is null)
+                return false;
+            _typeRepository.Delete(id);
+                return true;
+        }
+
+        public bool Update(TypeModele type)
+        {  
+            var oldType = _typeRepository.Get(type.Id);
+            if (oldType is null)
+            {
+                return false;
+            }
+            else
+            {
+                oldType.Nom = type.Nom;
+                oldType.Description = type.Description;
+                oldType.ObjetConcerne = type.ObjetConcerne;
+
+                _typeRepository.Update(oldType);
+                return true;
+            }
+        }
     }
 }
