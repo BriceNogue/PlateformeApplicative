0b6a10a59dc31702ec70552fa6a37d4b8e0b238f
BriceNogue <bricenogue@gmail.com>
2024-02-01 01:16:58 +0100

    creation du controleur TypeControler.

--- Domain/Entities/Etablissement.cs
+++ Domain/Entities/Etablissement.cs
@@ -1,6 +1,4 @@
 ﻿
-using Microsoft.EntityFrameworkCore;
-
 namespace Domain.Entities
 {
     public class Etablissement
---
+++ PlateformeApplicative/Controllers/TypeController.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlateformeApplicative.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypeController : ControllerBase
+    {
+        private readonly TypeService _typeService;
+
+        public TypeController()
+        {
+            _typeService = new TypeService();
+        }
+
+        [HttpGet("types")]
+        public ActionResult<List<TypeE>> GetAll()
+        {
+            List<TypeE> types = new List<TypeE>();
+            types = _typeService.GetAll();
+            if (types.Count == 0)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(types);
+            }
+        }
+
+        [HttpGet("type/id")]
+        public ActionResult<TypeE> Get(int id)
+        {
+            TypeE typeE = new TypeE();
+            typeE = _typeService.Get(id);
+            if (typeE is null)
+            {
+                return NotFound("Type not found.");
+            }
+            else
+            {
+                return Ok(typeE);
+            }
+        }
+    }
+}
