713b90df14214f45518a0b9e1e0891370eb7aa1b
BriceNogue <bricenogue@gmail.com>
2024-02-03 13:35:20 +0100

    Creation du SallesController

---
+++ PlateformeApplicative/Controllers/SallesController.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlateformeApplicative.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SallesController : ControllerBase
+    {
+        private readonly SalleService _service;
+
+        public SallesController() 
+        {
+            _service = new SalleService();
+        }
+
+        [HttpGet("all")]
+        public ActionResult<List<Salle>> GetAll()
+        {
+            var result = _service.GetAll();
+            if (result is null)
+                return NoContent();
+            return result;
+        }
+    }
+}
