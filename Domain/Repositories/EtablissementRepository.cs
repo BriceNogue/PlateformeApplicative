fcc11f26ecf5fec90e15ba1b4f7ec9a0d2f27561
BriceNogue <bricenogue@gmail.com>
2024-02-03 15:53:08 +0100

    Gestionn des exceptions dans etablisemtìent repository.

--- Domain/Repositories/EtablissementRepository.cs
+++ Domain/Repositories/EtablissementRepository.cs
@@ -13,31 +13,66 @@ namespace Domain.Repositories
 
         public List<Etablissement> GetAll()
         {
-            return _dataContext.Etablissements.ToList();
+            try
+            {
+                return _dataContext.Etablissements.ToList();
+            }
+            catch(Exception ex)
+            {
+               throw new Exception(ex.Message);
+            }
         }
-
         public Etablissement Get(int id)
         {
-            return _dataContext.Etablissements.FirstOrDefault(e => e.Id == id)!;
+            try
+            {
+                return _dataContext.Etablissements.FirstOrDefault(e => e.Id == id)!;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public void Add(Etablissement e)
         {
-            _dataContext.Etablissements.Add(e);
-            _dataContext.SaveChanges();
+            try
+            {
+
+                _dataContext.Etablissements.Add(e);
+                _dataContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public void Delete(int id)
         {
-            var etab = Get(id);
-            _dataContext.Etablissements.Remove(etab);
-            _dataContext.SaveChanges();
+            try
+            {
+                var etab = Get(id);
+                _dataContext.Etablissements.Remove(etab);
+                _dataContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public void Update(Etablissement e)
         {
-            _dataContext.Etablissements.Update(e);
-            _dataContext.SaveChanges();
+            try
+            {
+                _dataContext.Etablissements.Update(e);
+                _dataContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
