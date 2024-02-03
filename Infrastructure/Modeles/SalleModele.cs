574aaa261cc330c5239d184fe8ddd59ec3b3c3be
BriceNogue <bricenogue@gmail.com>
2024-02-03 13:18:58 +0100

    Mise a jour du SalleModele

--- Infrastructure/Modeles/SalleModele.cs
+++ Infrastructure/Modeles/SalleModele.cs
@@ -14,6 +14,6 @@ namespace Infrastructure.Modeles
         public string Capacite { get; set; } = string.Empty;
         public bool Status { get; set; }
         public int IdType { get; set; }
-        public int IdEtablissement { get; set; } = string.Empty;
+        public int IdEtablissement { get; set; }
     }
 }
