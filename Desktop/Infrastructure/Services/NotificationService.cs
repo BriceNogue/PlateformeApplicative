1f36bfb2bb059b9a7777294d50c7741823edd502
BriceNogue <bricenogue@gmail.com>
2024-02-09 00:38:11 +0100

    Changes

--- Desktop/Infrastructure/Services/NotificationService.cs
+++ Desktop/Infrastructure/Services/NotificationService.cs
@@ -8,5 +8,12 @@ namespace Desktop.Infrastructure.Services
 {
     public class NotificationService
     {
+        public NotificationService() { }
+
+        public void setTastBarNotification() 
+        {
+            // Créez l'icône de la barre des tâches
+            
+        }
     }
 }
