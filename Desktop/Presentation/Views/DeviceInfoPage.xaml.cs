60edf9a8d31252012572559e4c48c89f08b58c17
BriceNogue <bricenogue@gmail.com>
2024-02-15 15:53:41 +0100

    Changes

--- Desktop/Presentation/Views/DeviceInfoPage.xaml.cs
+++ Desktop/Presentation/Views/DeviceInfoPage.xaml.cs
@@ -66,11 +66,12 @@ namespace Desktop.Presentation.Views
             deviceViewModel.FreeROMSpace = used;
             deviceViewModel.UsedROMSpace = free;
 
-            txt_b_adr_ip.Text = _deviceInfoService.GetIPAddress();
-            txt_b_adr_mac.Text = _deviceInfoService.GetMACAddress();
-            txt_b_nom_poste.Text = _deviceInfoService.GetMachineName();
-            txt_b_marque.Text = _deviceInfoService.GetComputerManufacturer();
-            txt_b_os.Text = _deviceInfoService.GetOperatingSystem();
+            txt_adr_ip.Text = _deviceInfoService.GetIPAddress();
+            txt_adr_mac.Text = _deviceInfoService.GetMACAddress();
+            txt_nom_poste.Text = _deviceInfoService.GetMachineName();
+            txt_marque.Text = _deviceInfoService.GetComputerManufacturer();
+            txt_os.Text = _deviceInfoService.GetOperatingSystem();
+            txt_processeur.Text = _deviceInfoService.GetProcessor();
         }
 
     }
--- Shared/Modeles/PosteModele.cs
+++ Shared/Modeles/PosteModele.cs
@@ -14,6 +14,8 @@ namespace Shared.Modeles
         public string AdresseIp { get; set; } = string.Empty;
         public string AdresseMAC { get; set; } = string.Empty;
         public string SE { get; set; } = string.Empty;
+        public double ROM { get; set; }
+        public double RAM { get; set; }
         public int IdSalle { get; set; }
         public int IdType { get; set; }
         public bool Statut { get; set; }
