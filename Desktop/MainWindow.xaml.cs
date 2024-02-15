766fb378926cc20964f2866d794292060409f8dc
BriceNogue <bricenogue@gmail.com>
2024-02-15 08:42:14 +0100

    Changes

--- Desktop/MainWindow.xaml.cs
+++ Desktop/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Desktop.Presentation.Views;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Desktop
 {
@@ -8,9 +10,18 @@ namespace Desktop
     public partial class MainWindow : Window
     {
 
+        private Page currentPage = default!;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadPagesGrid.Navigate(new PostIndexPage());
+        }
+
+        private void ExitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
