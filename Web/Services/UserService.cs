74837b1d7737121847f9a0f9487d51ace5348bba
BriceNogue <bricenogue@gmail.com>
2024-03-13 17:35:22 +0100

    Changes

---
+++ Web/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Shareds.Modeles;
+
+namespace Web.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _URL = "";
+
+        public UserService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<UserModele>> GetAll()
+        {
+            List<UserModele>? res = await _httpClient.GetFromJsonAsync<List<UserModele>>(_URL + "/all");
+            if (res is not null)
+            {
+                return res;
+            }
+            else
+            {
+                return new List<UserModele>();
+            }
+        }
+    }
+}
