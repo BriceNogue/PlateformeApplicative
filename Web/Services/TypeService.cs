37d46866c975dfce65823abf1c177dd86514365c
BriceNogue <bricenogue@gmail.com>
2024-03-14 00:13:34 +0100

    Changes

--- Web/Services/TypeService.cs
+++ Web/Services/TypeService.cs
@@ -1,6 +1,28 @@
-﻿namespace Web.Services
+﻿using Shareds.Modeles;
+
+namespace Web.Services
 {
     public class TypeService
     {
+        private readonly HttpClient _httpClient;
+        private readonly string _URL = "https://localhost:7281/api/types";
+
+        public TypeService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task <List<TypeModele>> GetAll() {
+            List<TypeModele>? res = await _httpClient.GetFromJsonAsync<List<TypeModele>>(_URL + "/all");
+
+            if (res is not null)
+            {
+                return res;
+            }
+            else
+            {
+                return new List<TypeModele>();
+            }
+        }
     }
 }
