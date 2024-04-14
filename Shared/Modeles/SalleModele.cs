2016c114e5f61c0da1a1a81f15d29e1a66782e84
BriceNogue <bricenogue@gmail.com>
2024-04-14 20:21:54 +0200

    Poste index

--- Shared/Modeles/SalleModele.cs
+++ Shared/Modeles/SalleModele.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace Shareds.Modeles
     public class SalleModele
     {
         public int Id { get; set; }
+
+        [Required]
         public int Numero { get; set; }
+        [Required]
         public int Capacite { get; set; }
         public bool Status { get; set; }
+        [Required]
         public int IdType { get; set; }
         public int IdEtablissement { get; set; }
     }
