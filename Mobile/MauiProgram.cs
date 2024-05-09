0e0662d0a3db92dcbcca221f6f2e22c429d56777
BriceNogue <bricenogue@gmail.com>
2024-05-09 23:49:51 +0200

    Login page

--- Mobile/MauiProgram.cs
+++ Mobile/MauiProgram.cs
@@ -24,6 +24,7 @@ namespace Mobile
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFontAwesomeIconFonts();// UraniumUI
                 })
             #region /// Package UraniumUI
                 .UseUraniumUI()
--- Mobile/Views/vRegister.xaml.cs
+++ Mobile/Views/vRegister.xaml.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace Mobile.Views;
 
 public partial class vRegister : ContentPage
@@ -5,5 +7,34 @@ public partial class vRegister : ContentPage
 	public vRegister()
 	{
 		InitializeComponent();
-	}
+
+    }
+
+	private async void ToggleDisplayBorder(object sender, EventArgs e)
+	{
+		if(border_two.HeightRequest == 0)
+		{
+            border_one.HeightRequest = 0;
+            border_two.HeightRequest = 400;
+
+            indicator_one.Background = Colors.White;
+            indicator_two.Background = Color.Parse("#6074C6");
+
+            await border_two.ScaleTo(-0.1, 200);
+            await border_two.ScaleTo(1, 200);
+        }
+        else
+        {
+            border_two.HeightRequest = 0;
+            border_one.HeightRequest = 400;
+
+            indicator_one.Background = Color.Parse("#6074C6");
+            indicator_two.Background = Colors.White;
+
+            await border_one.ScaleTo(-0.1, 200);
+            await border_one.ScaleTo(1, 200);
+        }
+
+
+    }
 }
