3ac8716dfdc38527ab7712e0729f9dd9ec15963c
BriceNogue <bricenogue@gmail.com>
2024-05-11 18:52:19 +0200

    Register and login method in UserService

--- Mobile/Services/UserService.cs
+++ Mobile/Services/UserService.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using Shareds.Modeles;
 using System.Text;
-using System.Threading.Tasks;
+using static Shareds.Modeles.ResponsesModels;
 
 namespace Mobile.Services
 {
@@ -16,6 +15,65 @@ namespace Mobile.Services
             _httpClient = new HttpClient();
         }
 
-        
+        // Pour addapter le contenu de la requete
+        private HttpContent SetRequestContent(Object obj)
+        {
+            string json = JsonConvert.SerializeObject(obj);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return content;
+        }
+
+        public async Task<LoginResponse> Register(UserModele user)
+        {
+            var content = SetRequestContent(user);
+            HttpResponseMessage response = await _httpClient.PostAsync(_URL + "/register", content);
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var res = JsonConvert.DeserializeObject<LoginResponse>(responseBody)!;
+
+            return res;
+        }
+
+        public async Task<LoginResponse> Login(UserLoginModele loginM)
+        {
+            try
+            {
+                var userLogin = new LoginResponse(false, null, string.Empty);
+
+                StringContent content = (StringContent)SetRequestContent(loginM);
+
+                HttpResponseMessage response = await _httpClient.PostAsync(_URL + "/login", content);
+
+                if(response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    userLogin = JsonConvert.DeserializeObject<LoginResponse>(responseBody)!;
+
+                    if (userLogin.Flag)
+                    {
+                        if (userLogin.Token!.StartsWith("Bearer "))
+                        {
+                            string userToken = userLogin.Token!.Substring("Bearer ".Length);
+                            //SaveUserToken(userToken);
+                        }
+                        else
+                        {
+                            //SaveUserToken(userLogin.Token);
+                        }
+                    }
+                }
+
+                return userLogin;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null!;
+            }
+        }
     }
 }
