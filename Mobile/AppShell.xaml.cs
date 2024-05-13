00d15643bff58482175391fcef8beef77312aee7
BriceNogue <bricenogue@gmail.com>
2024-05-13 08:59:02 +0200

    Changes

--- Mobile/AppShell.xaml.cs
+++ Mobile/AppShell.xaml.cs
@@ -1,10 +1,42 @@
-﻿namespace Mobile
+﻿using Mobile.Domain.Entities;
+using Mobile.Domain.Repositories;
+
+namespace Mobile
 {
     public partial class AppShell : Shell
     {
+        UserSessionRepository userSR;
+        UserSession userSession;
+
         public AppShell()
         {
             InitializeComponent();
+
+            userSR = new UserSessionRepository();
+
+            GetUserSession();
+        }
+
+        public async void GetUserSession()
+        {
+            var res = await userSR.GetUserSession();
+            if (res != null)
+            {
+                userSession = new UserSession();
+                userSession = res;
+
+                txt_user_name.Text = userSession.Name;
+                txt_user_role.Text = userSession.Role;
+            }
+        }
+
+        public async void Logout(object sender, EventArgs e)
+        {
+            if (userSession != null)
+            {
+                await userSR.DeleteUserSession(userSession);
+                await Navigation.PushAsync(new MainPage());
+            }
         }
     }
 }
