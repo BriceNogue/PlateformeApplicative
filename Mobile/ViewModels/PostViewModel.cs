e3ef1c658bd20bac1e7f98433f2e028c772b5aad
BriceNogue <bricenogue@gmail.com>
2024-06-11 00:02:56 +0200

    Poste view

---
+++ Mobile/ViewModels/PostViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Shareds.Modeles;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mobile.ViewModels
+{
+    class PostViewModel : PosteModele, INotifyPropertyChanged
+    {
+        private bool isLabelVisible;
+        private bool isContentVisible;
+
+        private List<PosteModele> postes;
+
+        public bool IsLabelVisible
+        {
+            get { return isLabelVisible; }
+            set
+            {
+                if (isLabelVisible != value)
+                {
+                    isLabelVisible = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool IsContentVisible
+        {
+            get { return isContentVisible; }
+            set
+            {
+                if (isContentVisible != value)
+                {
+                    isContentVisible = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public List<PosteModele> Postes
+        {
+            get { return postes; }
+            set
+            {
+                if (postes != value)
+                {
+                    postes = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
